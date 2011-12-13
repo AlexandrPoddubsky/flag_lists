4fc160920487f9819f25fdbd99676e5442e7bf0b
Brandon Shi <brandon@imagexmedia.com>
2011-12-13 12:21:06 -0800

    making removing op much UI friendlier

--- js/flag_lists_ops.js
+++ js/flag_lists_ops.js
@@ -1,11 +1,11 @@
 (function ($) {
   Drupal.behaviors.flagListsOps = {
-    attach: function() {
+    attach: function(context) {
       // Hide the go button, as it is not needed for JS enabled browsers.
-      $('.flag-lists-ops-go').hide();
+      $('.flag-lists-ops-go', context).hide();
 
       // Make select all checkbox work
-      $('input.flo-table-select-all').each(function(i) {
+      $('input.flo-table-select-all', context).each(function(i) {
         var selectall = $(this);
 
         if (!selectall.hasClass('processed')) {
@@ -13,6 +13,14 @@
             $('input.flo-select', $(this).parents('form')).attr('checked', $(this).attr('checked'));
           }).addClass('processed');
         }
+      });
+
+      // Animate the deletion for AJAX deleting.
+      $('.flo-deleted-value', context).each(function(i) {
+        var parent = $(this).parents('.view');
+        $('.flo-select[value='+$(this).val()+']', parent).each(function(i) {
+          $(this).parents('.views-row, tr').fadeOut();
+        });
       });
     }
   }
